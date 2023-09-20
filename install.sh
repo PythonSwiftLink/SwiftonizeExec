52da37fa42bab02fdf97bb1c6af2d82f7432a2b8
psychowasp <psychowaspx@gmail.com>
2023-09-20 16:21:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env zsh
 
+BIN=/usr/local/bin/Swiftonize
 
+# git clone https://github.com/PythonSwiftLink/SwiftonizeExec
+# cd SwiftonizeExec
 xcodebuild \
     -project 'SwiftonizeExec.xcodeproj' \
     -config Release \
     -scheme 'SwiftonizeExec' \
     -archivePath ./archive archive
+
+sudo mkdir $BIN
+sudo cp -Rf python_stdlib $BIN/python_stdlib
+sudo cp -Rf python-extra $BIN/python-extra
+sudo cp -f archive.xcarchive/Products/usr/local/bin/SwiftonizeExec $BIN/SwiftonizeExec

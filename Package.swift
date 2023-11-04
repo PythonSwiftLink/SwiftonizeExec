92c9e0da2875b314cf914fadbdc1eefacce00b9d
psychowasp <psychowaspx@gmail.com>
2023-11-04 17:54:21 +0100

    fix

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/PythonSwiftLink/PythonSwiftLink", from: .init(311, 0, 0)),
 		//.package(path: "../PythonSwiftLink"),
-		.package(path: "../Swiftonize"),
-		//.package(url: "https://github.com/PythonSwiftLink/Swiftonize", branch: "testing"),
+		//.package(path: "../Swiftonize"),
+		.package(url: "https://github.com/PythonSwiftLink/Swiftonize", branch: "testing"),
 		
 		.package(url: "https://github.com/kylef/PathKit", from: .init(1, 0, 0) ),
 		.package(url: "https://github.com/apple/swift-syntax", from: .init(508, 0, 0) ),
